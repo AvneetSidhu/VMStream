8263bdf10e2103551499a30998ae98c35df7c21d
Avneet Sidhu <avneet.sidhu@ryerson.ca>
2025-05-12 02:38:44 -0400

    project structure & message struct

--- signal/main.go
+++ signal/main.go
@@ -4,7 +4,15 @@ import (
 	"fmt"
 	"github.com/gorilla/websocket"
 	"net/http"
+	"encoding/json"
+	"bytes"
 )
+
+type Message struct {
+	Type string `json:"type"`
+	ClientID string `json:"client_id"`
+	Payload string `json:"payload"`
+}
 
 var upgrader = websocket.Upgrader {
 	CheckOrigin: func(r *http.Request) bool {
@@ -12,23 +20,50 @@ var upgrader = websocket.Upgrader {
 	},
 }
 
-funct wsHandler(w http.ResponseWriter, r *http.Request) {
+func wsHandler(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		fmt.Println("Error upgrading connection:", err)
 		return
 	}
-	defer conn.Close()
+	defer conn.Close()    
 
 	for {
-		_, msg, err := conn.ReadMessage()
+		_, msgBytes, err := conn.ReadMessage()
 		if err != nil {
 			fmt.Println("Error reading message:", err)
 			break
 		}
-		fmt.Printf("Received message: %s\n", msg)
 
-		err = conn.WriteMessage(websocket.TextMessage, msg)
+		// fmt.Printf("Received message: %s\n", msgBytes)
+
+		var raw map[string]interface{}
+
+		if err := json.Unmarshal(msgBytes, &raw); err != nil {
+			fmt.Println("invalid JSON:", err)
+			break
+		}
+
+		for _, field := range []string{"type", "client_id", "payload"} {
+			if _, ok := raw[field]; !ok {
+				fmt.Printf("missing field: %s\n", field)
+				return
+			}
+		}
+
+		var msg Message
+
+		decoder := json.NewDecoder(bytes.NewReader(msgBytes))	
+		decoder.DisallowUnknownFields()
+
+		if err := decoder.Decode(&msg); err != nil {
+			fmt.Println("Struct parse error:", err)
+			break
+		}
+
+		// fmt.Printf("Received message: Type=%s, ClientID=%s, Payload=%s\n", msg.Type, msg.ClientID, msg.Payload)
+
+		err = conn.WriteMessage(websocket.TextMessage, msgBytes)
 		if err != nil {
 			fmt.Println("Error writing message:", err)
 			break
