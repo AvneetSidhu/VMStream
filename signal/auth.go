ef6923babf652566f9e46b8bd307095052a01ab9
Avneet Sidhu <avneet.sidhu@ryerson.ca>
2025-08-28 19:47:05 -0400

    revert

--- signal/auth.go
+++ signal/auth.go
@@ -60,7 +60,7 @@ func issueRefreshTokenCookie(clientID string) (http.Cookie, error) {
 		Value:   refreshTokenString,
 		Path:    "/api/refresh",
 		HttpOnly: true,
-		Secure:   false,
+		Secure:   true,
 		SameSite: http.SameSiteLaxMode,
 		Expires:  time.Now().Add(24 * time.Hour),
 	}
